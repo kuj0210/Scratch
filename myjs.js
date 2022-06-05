f68acf476ce9b435db421b602bad3a399368277d
woojin <33398268+kuj0210@users.noreply.github.com>
2022-06-05 23:05:56 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,8 +1,8 @@
 
 
-L= new Array();
+Q= new Array();
 for(var i=0 ; i<5; i++){
     var key ="l"+(i+1).toString(); 
-    L[i] = document.getElementById(key);
-    L[i].innerText= "min"+i.toString();
+    Q[i] = document.getElementById(key);
+    Q[i].innerText= "min"+i.toString();
 }
