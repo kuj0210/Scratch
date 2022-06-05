43157b9a27f46c1006a843e100f90131dd8986bd
woojin <33398268+kuj0210@users.noreply.github.com>
2022-06-05 22:58:32 +0900

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,13 @@
+
+
+L= new Array();
+L[0] = document.getElementById("l1");
+L[1] = document.getElementById("l2");
+L[2] = document.getElementById("l3");
+L[3] = document.getElementById("l4");
+L[4] = document.getElementById("l5");
+
+for(var i=0 ; i<5; i++){
+    L[i].innerText= "min"+i.toString();
+
+}
