5aca83d9714856e370343470766469edfda412d5
woojin <33398268+kuj0210@users.noreply.github.com>
2022-06-15 00:33:22 +0900

    Add files via upload

---
+++ integerPlusWeb/keyMaker.js
@@ -0,0 +1,41 @@
+num= "586723";
+key = Array.from(num);
+len = key.length;
+opener = new Array();
+var i =0;
+var flag=0;
+
+var j=0;
+while(true){
+    flag =  Math.floor(Math.random() * 6) ;
+ if(opener.indexOf(flag)==-1)// 새로운 수라면
+    {
+        opener[i]=flag;//추가
+        i++;
+            
+    }
+    if(i>=2) break; //2개 생성했다면 out
+}
+console.log(opener[0]);
+console.log(opener[1]);
+
+for(i=0; i< len ; i++){
+    console.log("i번째: "+i);
+    if(opener.indexOf(i)==-1){
+        console.log("비공개");
+        str = "( ? )"
+        document.write(str);
+    }
+    else{
+        console.log("공개");
+        str = "( "+key[i]+" )";
+        document.write(str);
+
+    }
+
+}
+    
+
+
+ 
+    
---
+++ moveController.js
@@ -0,0 +1,9 @@
+//문제지 주소
+const W1="./integerPlusWeb/intplus.html";  //정수 덧셈 링크
+
+
+
+
+var link = W1;
+location.href=link;location.replace(link);
+window.open(link);
