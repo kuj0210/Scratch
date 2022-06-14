09cbd8485d218044d631ef9fd222b60355a3c73d
woojin <33398268+kuj0210@users.noreply.github.com>
2022-06-15 00:36:10 +0900

    Update moveController.js

--- moveController.js
+++ moveController.js
@@ -3,7 +3,7 @@ const W1="./integerPlusWeb/intplus.html";  //정수 덧셈 링크
 
 
 
-
 var link = W1;
-location.href=link;location.replace(link);
-window.open(link);
+location.href=link;
+location.replace(link);//이동
+//window.open(link); 새창열기
